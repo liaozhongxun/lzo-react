d0d804d82871a02c51b996d97385acdf26f4c8bc
lenovo-kali <869664233@qq.com>
2022-12-21 08:19:05 +0800

    2022-12-21 08:19提交上传

--- src/components/BrotherOne.js
+++ src/components/BrotherOne.js
@@ -4,20 +4,54 @@ export class BrotherOne extends Component {
     constructor() {
         super()
         this.state = {
-            name: "default"
+            name: "default",
+            isChecked: true,
+            selectType: 'banana'
         }
     }
 
     changeInput(e) {
-        console.log(e.target.value)
+        // this.setState({
+        //     name: e.target.value
+        // })
+        // 或 批量写法
+
+        if (e.target.name === 'checkbox') {
+            console.log(e.target.value) // on | off
+            console.log(e.target.checked) // true | false
+            this.setState({
+                // isChecked: !this.state.isChecked
+                isChecked: e.target.checked
+            })
+        } else {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    changeSelect(e) {
         this.setState({
-            name: e.target.value
+            selectType: e.target.value
         })
     }
 
     render() {
         return (
-            <input value={this.state.name} onChange={e => this.changeInput(e)} />
+            <div>
+                <input value={this.state.name} name='name' onChange={e => this.changeInput(e)} />
+                <label>
+                    <input type='checkbox' checked={this.state.isChecked} onChange={e => this.changeInput(e)} />
+                </label>
+                <div>
+                    <select value={this.state.selectType} onChange={e => this.changeSelect(e)}>
+                        <option value='apple'>苹果</option>
+                        <option value='banana'>香蕉</option>
+                        <option value='origin'>橘子</option>
+                    </select>
+                </div>
+                <input defaultValue={this.state.name} />
+            </div>
         )
     }
 }
